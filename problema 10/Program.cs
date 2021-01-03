1d1be83deca8f2f3887fa993ac382d65b3748420
Carina Rebeca <carina.plisco@gmail.com>
2021-01-04 01:00:21 +0200

    cautare binara

---
+++ problema 10/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cautare binara. Se da un vector cu n elemente sortat in ordine crescatoare. 
+            // Se cere sa se determine pozitia unui element dat k. 
+            // Daca elementul nu se gaseste in vector rezultatul va fi -1.
+
+            int n, i, j, aux, k;
+
+            Console.WriteLine(" Cate elemente va avea vectorul? ");
+            Console.Write(" n = ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] v = new int[n];
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write($" v[{i}] = ");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (i = 0; i < n - 1; i++)
+            {
+                for (j = i + 1; j < n; j++)
+                {
+                    if (v[i] > v[j])
+                    {
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            }
+            Console.WriteLine(" Vectorul sortat in ordine crescatoare este: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine($" {v[i]} ");
+            }
+
+            Console.Write(" k = ");
+            k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($" Prin cautarea binara, elementul {k} este pe pozitia {cautarebinara(v, k, 0, n-1)}. ");
+        }
+        private static int cautarebinara(int[] v, int k, int s, int d)
+        {
+            int m;
+            m = (s + d) / 2;
+            if (s > d)
+                return -1;
+            else
+            {
+                if (k == v[m])
+                    return m;
+                if (k < v[m])
+                    return cautarebinara(v, k, s, m - 1);
+                else
+                    return cautarebinara(v, k, m + 1, d);
+            }
+        }
+    }
+}
+
+
