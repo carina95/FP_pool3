416e60372c180f5ce2c3b65261f44dfc7596621c
Plisco Carina <72436247+carina95@users.noreply.github.com>
2021-01-18 16:38:08 +0200

    cautare binara

--- problema 10/Program.cs
+++ problema 10/Program.cs
@@ -9,7 +9,7 @@ namespace problema_10
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             // Cautare binara. Se da un vector cu n elemente sortat in ordine crescatoare. 
             // Se cere sa se determine pozitia unui element dat k. 
             // Daca elementul nu se gaseste in vector rezultatul va fi -1.
