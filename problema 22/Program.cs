05d0b0bd4b8c080f294b424b63a7793ebf95281d
Carina Rebeca <carina.plisco@gmail.com>
2021-01-21 01:07:14 +0200

    intersectia, reuniunea

--- problema 22/Program.cs
+++ problema 22/Program.cs
@@ -16,7 +16,7 @@ namespace problema_22
             // (implementarea operatiilor cu multimi). 
             // Elementele care se repeta vor fi scrise o singura data in rezultat. 
 
-            int n, l, i, j;
+            int n, l, i, j, k, p, ps;
 
             Console.WriteLine(" Cate elemente va avea vectorul v1 ?");
             Console.Write(" n = ");
@@ -50,16 +50,46 @@ namespace problema_22
 
             Console.WriteLine($" v1 intersectat cu v2 : {{ {s} }} ");
 
-            string r = "";
+            p = v1.Length + v2.Length;
+            int[] v = new int[p];
 
-            Console.WriteLine($" v1 reunit cu v2 : ");
-
-            for ( j = 0; j < v2.Length; j++)
+            i = 0; 
+            j = 0;
+            k = 0;
+            ps = i;
+            while (i < v1.Length && j < v2.Length)
             {
-                for ( i = 0; i < v1.Length; i++)
-                    if (v1[j] != 0 && v2[j] != 0)
-                        Console.Write(j + " ");
+                if (v1[i] < v2[j])
+                {
+                    v[ps + k] = v1[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    v[ps + k] = v2[j];
+                    j++;
+                    k++;
+                }
             }
+            while (i < v1.Length)
+            {
+                v[ps + k] = v1[i];
+                i++;
+                k++;
+            }
+            while (j < v2.Length)
+            {
+                v[ps + k] = v2[j];
+                j++;
+                k++;
+            }
+            for ( i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine($" {v[i]} ");
+            }
+            
         }
     }
+
 }
